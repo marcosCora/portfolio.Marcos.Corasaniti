a6e73905314d0295597ff1e503309b00a2999deb
Marcos Corasaniti <marcoscorasniti07@gmail.com>
2026-02-05 11:17:31 -0300

    favicon

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,24 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { concat } from 'rxjs';
+import { Contact } from './sections/contact/contact';
+import { Hero } from './sections/hero/hero';
+import { Mindset } from './sections/mindset/mindset';
+import { Personal } from './sections/personal/personal';
+import { Projects } from './sections/projects/projects';
+import { Header } from "./sections/header/header";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    Contact,
+    Hero,
+    Mindset,
+    Personal,
+    Projects,
+    Header
+],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/sections/mindset/mindset.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mindset',
+  imports: [],
+  templateUrl: './mindset.html',
+  styleUrl: './mindset.css',
+})
+export class Mindset {
+
+}
